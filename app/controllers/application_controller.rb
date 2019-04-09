311dbe95e14689535ee4c1b326ecdf1b85dc49bb
Lizzie <lchan217@gmail.com>
2019-04-09 08:33:23 -0500

    user controller complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,15 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    "Herro World"
+    erb :'/index'
   end
 
+  helpers do
+    def logged_in?
+       !!session[:user_id]
+    end
+    def current_user
+       User.find(session[:user_id])
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
-end 
+  get '/signup' do
+      erb :'/users/create_user'
+  end
+  post '/signup' do
+    @name = params[:name]
+    @username = params[:username]
+    @email = params[:email]
+    @password = params[:password]
+
+    unless @name.empty? || @username.empty? || @email.empty? || @password.empty?
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      @locations = Location.all
+      erb :'/locations/index'
+    else
+      erb :'users/error'
+    end
+  end
+
+  get '/login' do
+      erb :'/users/login'
+  end
+  post '/login' do
+     @user = User.find_by(username: params[:username])
+     if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        @locations = Location.all
+        erb :'/locations/index'
+     else
+        erb :'/users/error'
+     end
+  end
+end
