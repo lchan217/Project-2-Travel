5be8e9deea659f11bb42222bc640d1b8ba1ac93a
Lizzie <lchan217@gmail.com>
2019-04-09 10:43:33 -0500

    delete function works

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -15,6 +15,26 @@ class LocationsController < ApplicationController
     @location = Location.find(params[:id])
     erb :'/locations/show'
   end
-
+  get '/locations/:id/edit' do
+    erb :'/locations/edit'
+  end
+  patch '/locations/:id' do
+    @location = Location.find(params[:id])
+    @city = params[:city]
+    @country = params[:country]
+    @attraction = params[:tourist_attraction]
+    @date = params[:date_visited]
+    unless @location.empty? || @city.empty? || @attraction.empty? || @date.empty?
+      params.delete("_method")
+      @location.update(params)
+      redirect "/locations/#{@location.id}"
+    else
+      redirect "/locations/#{@location[:id]}/edit"
+    end
+  end
+  delete '/locations/:id/delete' do
+   Location.find(params[:id]).destroy
+   erb :'/locations/delete'
+  end
 
 end
