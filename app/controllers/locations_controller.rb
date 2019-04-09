c4269823cf8b1def83d63830e839d6ae18d8ba7d
Lizzie <lchan217@gmail.com>
2019-04-09 09:20:42 -0500

    user an add a new from index

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,13 @@
 class LocationsController < ApplicationController
   get '/index' do
   end
+  get '/locations/new' do
+    erb :'/locations/new'
+  end
   get '/locations/:id' do
     @location = Location.find(params[:id])
     erb :show
   end
+
+
 end
