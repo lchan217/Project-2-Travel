e09091e8244caa5b314c5fdc3f12a4705c04b97e
Lizzie <lchan217@gmail.com>
2019-04-11 20:09:37 -0500

    duplicate pw error fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true
   validates :name, presence: true
-  validates :password, presence: true
   validates :username, presence: true
   validates :email, uniqueness: true
   validates :username, uniqueness: true
